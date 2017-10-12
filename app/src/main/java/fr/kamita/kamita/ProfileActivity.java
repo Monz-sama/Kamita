64acd8036f0bcf20aea60ccfe22962ac66d750d0
Monzon DIOP <monzon.diop@capgemini.com>
2017-10-12 17:52:50 +0200

    adding register page

--- app/src/main/java/fr/kamita/kamita/ProfileActivity.java
+++ app/src/main/java/fr/kamita/kamita/ProfileActivity.java
@@ -1,7 +1,12 @@
 package fr.kamita.kamita;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -9,5 +14,19 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        final EditText inputEmail = (EditText) findViewById(R.id.input_email);
+        final EditText inputPassword = (EditText) findViewById(R.id.input_password);
+        final Button btnLogin = (Button) findViewById(R.id.btn_login);
+        final TextView registerLink = (TextView) findViewById(R.id.link_signup);
+
+        registerLink.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(ProfileActivity.this,RegisterActivity.class);
+                startActivity(registerIntent);
+            }
+        });
+
     }
 }
